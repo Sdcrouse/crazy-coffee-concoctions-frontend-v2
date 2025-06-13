13a004c46b11eeeaa5fbda22a8b075f1b6617d80
Steven Crouse <sdcrouse91@gmail.com>
2025-06-12 23:11:54 -0700

    Added and styled a home page element and adjusted styles for other elements

---
+++ src/index.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const mainContainer = document.getElementById('main-container');
+    
+    mainContainer.innerHTML = `
+        <div>
+            <h2>Welcome to the Crazy Coffee Concoctions app!</h2>
+            <p>Here, you can create and save your favorite coffee combinations.</p>
+        </div>
+    `;
+});
