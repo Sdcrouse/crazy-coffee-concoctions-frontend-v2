c15e0ca898b3b82934c45419130d4a99a043ea2a
Steven Crouse <sdcrouse91@gmail.com>
2025-06-13 23:14:47 -0700

    Added a Home button that takes users back to the homepage
    This also updates the instructions when the user clicks the 'Brew Coffee With a Teapot' button.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import generateHomePage from './components/home.js';
 
 document.addEventListener("DOMContentLoaded", () => {
     generateHomePage();
+
+    const homeBtn = document.getElementById('home-btn');
+    homeBtn.addEventListener('click', generateHomePage);
     
     const coffeeTeapotBtn = document.getElementById('coffee-with-teapot-btn');
     coffeeTeapotBtn.addEventListener('click', () => displayErrorImage(418));
@@ -14,7 +17,7 @@ function displayErrorImage(httpStatus) {
         <img id="im-a-teapot" src="img/${httpStatus}-im-a-teapot.png" alt="${httpStatus} I'm a teapot" />
         <h2>Sorry! The server is currently unable to brew coffee at the moment because it is now a teapot. You can't brew coffee with a teapot!</h2>
         <p>
-            To change the teapot back into a server, please refresh the page.
+            To change the teapot back into a server, please click the "Home" button on the left or refresh the page.
             Or you can visit <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status/418">this handy MDN article</a>
             to learn more about this fun little HTTP status code.
         </p>
