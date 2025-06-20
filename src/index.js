0079ea2c4ced40d0959b11e9771931135b39f416
Steven Crouse <sdcrouse91@gmail.com>
2025-06-20 13:41:59 -0700

    Refactored the displayErrorImage function without using innerHTML
    InnerHTML takes longer to build an element and is less safe.
    This commit builds the error image and related info piece-by-piece.
    This should also make it easier to pass in a different error image later.

--- src/index.js
+++ src/index.js
@@ -12,9 +12,7 @@ const signupButton = document.getElementById('signup');
 
 initialPageSetup();
 
-function initialPageSetup() {
-    generateHomePage();
-    
+function initialPageSetup() {    
     homeBtn.addEventListener('click', generateHomePage);
     signupButton.addEventListener('click', generateSignupPage);
     coffeeTeapotBtn.addEventListener('click', () => displayErrorImage(418));
@@ -22,28 +20,60 @@ function initialPageSetup() {
 
 function generateHomePage() {
     titleElement.textContent = homePageTitle;
-    mainContainer.innerHTML = homePageContent;
+    mainContainer.innerHTML = homePageContent; // I want to replace this if possible
 }
 
 function displayErrorImage(httpStatus) {
-    const mainContainer = document.getElementById('main-container');
-    
-    mainContainer.innerHTML = `
-        <img id="im-a-teapot" src="img/${httpStatus}-im-a-teapot.png" alt="${httpStatus} I'm a teapot" />
-        <h2 id="teapot-image-heading" class="extra-padding coffee-text">Sorry! The server is currently unable to brew coffee at the moment because it is now a teapot. You can't brew coffee with a teapot!</h2>
-        <p class="extra-padding">
-            To change the teapot back into a server, please click the "Home" button on the left or refresh the page.
-            Or you can visit <a href="https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status/418">this handy MDN article</a>
-            to learn more about this fun little HTTP status code.
-        </p>
-        <hr />
-        <p id="teapot-image-info" class="extra-padding">
-            <small>
-                &copy; 2025 "${httpStatus}" image courtesy of <a href="https://www.drlinkcheck.com/blog/free-http-error-images">Dr. Link Check</a><br>
-                It is available for download free of charge under the <a href="https://creativecommons.org/licenses/by/4.0/legalcode">Creative Commons CC BY 4.0 license</a>
-            </small>
-        </p>
+    titleElement.textContent = `${httpStatus} - I'm a Teapot!`;
+
+    const teapotImage = document.createElement('img');
+    teapotImage.setAttribute('id', 'im-a-teapot');
+    teapotImage.setAttribute('src', `img/${httpStatus}-im-a-teapot.png`);
+    teapotImage.setAttribute('alt', `${httpStatus} I'm a teapot`);
+
+    const teapotHeading = document.createElement('h2');
+    teapotHeading.setAttribute('id', 'teapot-image-heading');
+    teapotHeading.className = 'extra-padding coffee-text';
+    teapotHeading.textContent = `
+        Sorry! The server is currently unable to brew coffee at the moment because it is now a teapot. 
+        You can't brew coffee with a teapot!
     `;
 
-    titleElement.innerText = `${httpStatus} - I'm a Teapot!`;
+    const mdnArticleLink = document.createElement('a');
+    mdnArticleLink.setAttribute('href', 'https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Status/418');
+    mdnArticleLink.textContent = 'this handy MDN article';
+
+    let instructions = document.createElement('p');
+    instructions.className = 'extra-padding';
+    instructions.append(
+        'To change the teapot back into a server, please click the "Home" button on the left or refresh the page. Or you can visit ',
+        mdnArticleLink,
+        ' to learn more about this fun little HTTP status code.'
+    );
+
+    const errorImageSource = document.createElement('a');
+    errorImageSource.setAttribute('href', 'https://www.drlinkcheck.com/blog/free-http-error-images');
+    errorImageSource.textContent = 'Dr. Link Check';
+
+    const errorImageLicense = document.createElement('a');
+    errorImageLicense.setAttribute('href', 'https://creativecommons.org/licenses/by/4.0/legalcode');
+    errorImageLicense.textContent = 'Creative Commons CC BY 4.0 license';
+
+    const errorImageSmallText = document.createElement('small');
+    errorImageSmallText.append(
+        `\u00A9 2025 "${httpStatus}" image courtesy of `,
+        errorImageSource,
+        document.createElement('br'),
+        'It is available for download free of charge under the ',
+        errorImageLicense
+    );
+
+    const errorImageInfo = document.createElement('p');
+    errorImageInfo.setAttribute('id', 'teapot-image-info');
+    errorImageInfo.className = 'extra-padding';
+    errorImageInfo.appendChild(errorImageSmallText);
+
+    let errorFragment = new DocumentFragment();
+    errorFragment.append(teapotImage, teapotHeading, instructions, document.createElement('hr'), errorImageInfo);
+    mainContainer.replaceChildren(errorFragment);
 }
