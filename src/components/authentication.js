185ff5f4fc8751e28b968a2d3f2c02ad07759ca1
Steven Crouse <sdcrouse91@gmail.com>
2025-06-21 20:18:50 -0700

    Added signup TODOs and additional validations for the username and password

--- src/components/authentication.js
+++ src/components/authentication.js
@@ -79,6 +79,10 @@ function generateErrorList(errors) {
 }
 
 function signup(event, signupForm) {
+    // TODO: Make sure the username doesn't already exist
+    // TODO: Save the user to the backend database
+    // TODO: Render the login page with a success message when the user signs up
+
     event.preventDefault();
 
     const signupFormInputs = new FormData(signupForm);
@@ -86,13 +90,37 @@ function signup(event, signupForm) {
     let usernameErrors = [], passwordErrors = [];
 
     if (username) {
+        let usernameRegex = /^[\w\.]+$/;
+        if (!usernameRegex.test(username)) {
+            usernameErrors.push('Username must only contain letters, numbers, periods, and underscores');
+        }
+
+        usernameRegex = /^[a-zA-Z].*[a-zA-Z\d]$/;
+        if (!usernameRegex.test(username)) {
+            usernameErrors.push('Username must start with a letter and end with a letter or number');
+        }
+
         if (username.length < 8) { usernameErrors.push('Username must be at least eight characters long'); }
     } else {
         usernameErrors.push('Username is required');
     }
 
     if (password) {
+        const containsOneOfEach = 
+            /[a-zA-Z]+/.test(password) // letters
+            && /\d+/.test(password) // numbers
+            && /[!@#$%^&*_+=?<>,.]+/.test(password); // special characters
+
+        if (!containsOneOfEach) {
+            passwordErrors.push(`Password must contain one of each of the following types of characters: 
+                lower and/or uppercase letters (a-z, A-Z), numbers (0-9), and special characters (!@#$%^&*_+=?<>,.)`);
+        }
+
         if (password.length < 8) { passwordErrors.push('Password must be at least eight characters long'); }
+
+        if (password.includes(username) || password.toLowerCase().includes('password')) {
+            passwordErrors.push("Password must not contain the username or the word 'password' (either upper or lowercase)");
+        }
     } else {
         passwordErrors.push('Password is required');
     }
