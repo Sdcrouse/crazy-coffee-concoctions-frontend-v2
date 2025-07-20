294084620232a31370827d4886da50a3ec1cce18
Steven Crouse <sdcrouse91@gmail.com>
2025-07-19 18:00:14 -0700

    Updated the concoctions page with a heading and additional content formatting

--- src/components/concoctions.js
+++ src/components/concoctions.js
@@ -5,11 +5,30 @@ const titleElement = document.querySelector('title');
 const baseTitle = 'Crazy Coffee Concoctions';
 const apiBase = 'http://localhost:5000';
 
-export async function generateConcoctionsPage(successMessage = '') {
+export async function generateConcoctionsPage(loginSuccessMessage = '') {
     titleElement.textContent = `${baseTitle} - Your Concoctions`;
 
-    // TODO: Move this logic into a separate getConcoctions function
+    const concoctionsDiv = createCustomElement('div', { id: 'concoctions-div' });
+
+    if (loginSuccessMessage) {
+        const loginSuccessHeading = createCustomElement('h3', {
+            text: loginSuccessMessage,
+            classes: 'success-text center-content'
+        });
+        concoctionsDiv.appendChild(loginSuccessHeading);
+    }
+
+    const concoctionsHeading = createCustomElement('h2', {
+        text: 'Your Concoctions',
+        classes: 'center-content coffee-text'
+    });
+
+    concoctionsDiv.appendChild(concoctionsHeading);
+
     // TODO: Add logic for the different responses returned by the get /concoctions request (currently 200, 401, and 500)
+    // TODO: Move this logic into a separate getConcoctions function
+    // (Or I may wind up renaming this function and moving the logic for creating the concoctions page into a separate function)
+    // Just remember that I am ALSO sending this function a success message when the user logs in
     const newResponse = await fetch(`${apiBase}/concoctions`, {
         method: 'GET',
         headers: { 'Content-Type': 'application/json' },
@@ -17,15 +36,8 @@ export async function generateConcoctionsPage(successMessage = '') {
     });
     const newData = await newResponse.json();
 
-    mainContainer.replaceChildren();
-
-    if (successMessage) {
-        mainContainer.appendChild(
-            createCustomElement('h2', { text: successMessage, classes: 'success-text center-content' })
-        );
-    }
-
-    mainContainer.appendChild(
+    concoctionsDiv.appendChild(
         createCustomElement('p', { text: newData.message, classes: 'center-content' })
     );
+    mainContainer.replaceChildren(concoctionsDiv);
 };
